d8ed54571de72047c388334f514c582e0c8a755b
Billy Dominique <billydominique@pursuit.org>
2022-02-11 12:08:01 -0500

    updated app.js in the backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,26 @@
 // DEPENDENCIES
 
 const express = require("express");
+const cors = require('cors')
+const snackController = require("./controllers/snackController")
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use("/snacks", snackController);
+
 
 // ROUTES
+app.get("/", (request,response) => {
+    console.log("GET request to /");
+    response.send("Grab One or be be One");
+  });
 
+  app.get("*", (request, response) => {
+    response.status(404).json({ error: "Page not found" });
+  });
 // EXPORT
 module.exports = app;
