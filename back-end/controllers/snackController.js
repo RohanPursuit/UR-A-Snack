324480a9b768d13a4514ae2f61f98573e4586cf4
Rohan-Sterling <rohansterling@pursuit.org>
2022-02-11 16:32:08 -0500

    fix add key id to req.param object

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -23,13 +23,13 @@ snacks.post("/", async (req, res) => {
 
 snacks.delete("/:id", async (req, res) => {
     console.log("Delete /snacks/:id")
-    const snack = await deleteSnack(req.params)
+    const snack = await deleteSnack(req.params.id)
     res.status(200).json(snack) 
 })
 
 snacks.put('/:id', async (req, res) => {
     console.log("Put /:id")
-    const song = await editSnack(req.params, req.body)
+    const song = await editSnack(req.params.id, req.body)
     res.status(200).json(song)
 })
 
