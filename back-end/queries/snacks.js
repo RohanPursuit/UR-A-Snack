18cdf84c016f31b74d2e5830970ff8d46f7ad91e
Billy Dominique <billydominique@pursuit.org>
2022-02-11 15:46:51 -0500

    added createSnack, deleteSnack,  editSnack to queries

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -2,8 +2,7 @@ const db = require("../db/dbConfig.js");
 
 const getAllSnacks = async () => {
     try {
-      const allSnacks = await db.any("SELECT * FROM snacks ");
-      console.log(allSnacks);
+      const allSnacks = await db.any("SELECT * FROM snacks");
       return allSnacks;
     } catch (error) {
       return error;
@@ -19,7 +18,47 @@ const getAllSnacks = async () => {
     }
     }; 
 
+    const createSnack = async (snack) => {
+        try {
+            const {name, fiber, protein, added_sugar, is_healthy, image} = snack 
+            const newSnack = await db.one(
+            "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+            [name, fiber, protein, added_sugar, is_healthy, image]
+            );
+            return newSnack;
+        } catch (error) {
+            return error;
+        }
+        };
+
+        const deleteSnack = async (id) => {
+            try {
+                const deletedSnack = await db.one(
+                "DELETE FROM snacks WHERE id = $1 RETURNING *",
+                id
+                );
+                return deletedSnack;
+            } catch (error) {
+                return error;
+            }
+            };  
+
+            const editSnack = async (id, snack) => {
+                try {
+                    const {name, fiber, protein, added_sugar, is_healthy, image} = snack;
+             
+                  const updatedSnack = await db.one("UPDATE snacks SET name=$1, fiber=$2, protein=$3, added_sugar=$4, is_healthy=$5, image=$6  WHERE id=$7 RETURNING *",
+                  [name, fiber, protein, added_sugar, is_healthy, image, id] );
+                  return updatedSnack;
+                }catch(error){
+                  return error;
+                };
+                
+              }
 module.exports = {
     getAllSnacks,
-    getSnack
+    getSnack,
+    createSnack,
+    deleteSnack,
+    editSnack
 };
