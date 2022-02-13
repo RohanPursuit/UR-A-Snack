bb422d142cf30f996d9deb41fac0118229b767f6
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 09:58:33 -0500

    add comments for signin feature

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <NavBar/>
       <Routes>
+        //use home as Sign in screen
+        //Hide NavBar when on Sign in screen
         <Route exact path="/" element={<h2>Hello, world!</h2>}/>
       </Routes>
     </div>
