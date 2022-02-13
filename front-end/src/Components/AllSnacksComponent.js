294fad16cf02868ba97e3f5712071a59dca6406f
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 12:32:32 -0500

    import HeartHealth from ./HeartHealth, add URL env for path to local api, add state snacks, add useEffect with axios requesting all snacks, return an array of snack items.

--- front-end/src/Components/AllSnacksComponent.js
+++ front-end/src/Components/AllSnacksComponent.js
@@ -1,10 +1,29 @@
 import axios from "axios"
 import {useState, useEffect} from "react"
+import HeartHealth from "./HeartHealth"
 
 function AllSnacks() {
+  const URL = process.env.REACT_APP_API_URL
+  const [snacks, setSnack] = useState([])
+
+  useEffect(()=> {
+    axios.get(`${URL}/snacks`)
+    .then(response => {
+      setSnack(response.data)
+    })
+    .catch(console.log)
+  }, [URL])
     return (
       <div className="AllSnacks">
-        All Snacks
+        {snacks.map(el => {
+          return(
+            <div key={el.id} className="snack-card">
+              <img src={el.image} alt="" />
+              {<HeartHealth snackHealth={el.is_healthy}/>}
+              <p>{el.name}</p>
+            </div>
+          )
+        })}
       </div>
     );
 }
