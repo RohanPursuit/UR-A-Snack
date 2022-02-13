c13f4f54b709b6144fbb7b48195d6de5d1303338
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 11:15:44 -0500

    created EditSnackComponent.js and EditSnack component, import axios from axios and useState, useEffect from react

---
+++ front-end/src/Components/EditSnackComponent.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import {useState, useEffect} from "react"
+
+function EditSnack() {
+    return (
+      <div className="EditSnack">
+        Edit Snack
+      </div>
+    );
+}
+
+export default EditSnack
