3803107016833e9db28cb2c816f8128d97a40885
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 10:13:43 -0500

    import AllSnacksPage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import {Routes, Route} from "react-router-dom"
 import NavBar from "./Components/Common/NavBar";
+import AllSnacksPage from "./Pages/AllSnacksPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         //use home as Sign in screen
         //Hide NavBar when on Sign in screen
         <Route exact path="/" element={<h2>Hello, world!</h2>}/>
+        <Route path="/snacks" element={<AllSnacksPage/>}/>
       </Routes>
     </div>
   );
