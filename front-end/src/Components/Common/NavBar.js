322700c1d6e0a821ece609d6e44bf2e17a82a357
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 09:53:32 -0500

    add Link for show all snacks and Link for create new snack

--- front-end/src/Components/Common/NavBar.js
+++ front-end/src/Components/Common/NavBar.js
@@ -3,8 +3,11 @@ import {Link} from "react-router-dom"
 function NavBar() {
     return (
         <div className="NavBar">
-            <Link to="/">
+            <Link to="/snacks">
             Hello Nav Bar!
+            </Link>
+            <Link to="/snack/new">
+
             </Link>
         </div>
     );
