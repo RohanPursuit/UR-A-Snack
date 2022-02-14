c674ef368bd78ab65858be64ddaf57d7353273b7
RohanPursuit <rohansterling@pursuit.org>
2022-02-13 22:01:21 -0500

    add position fixed to navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,19 +3,21 @@ import NavBar from "./Components/Common/NavBar";
 import AllSnacksPage from "./Pages/AllSnacksPage";
 import SnackDetailPage from "./Pages/SnackDetailPage";
 import NewSnackPage from "./Pages/NewSnackPage"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Routes>
-        //use home as Sign in screen
-        //Hide NavBar when on Sign in screen
-        <Route exact path="/" element={<h2>Hello, world!</h2>}/>
-        <Route path="/snacks" element={<AllSnacksPage/>}/>
-        <Route path="/snacks/:id" element={<SnackDetailPage/>}/>
-        <Route path="/snacks/new" element={<NewSnackPage/>}/>
-      </Routes>
+      <div className="sidebar"></div>
+        <NavBar/>
+        <Routes>
+          //use home as Sign in screen
+          //Hide NavBar when on Sign in screen
+          <Route exact path="/" element={<h2>Hello, world!</h2>}/>
+          <Route path="/snacks" element={<AllSnacksPage/>}/>
+          <Route path="/snacks/:id" element={<SnackDetailPage/>}/>
+          <Route path="/snacks/new" element={<NewSnackPage/>}/>
+        </Routes>
     </div>
   );
 }
