696d399a4195841dc8719652f476070d907e27fb
RohanPursuit <rohansterling@pursuit.org>
2022-02-14 20:10:49 -0500

    import useParams, using id to get single snack

--- front-end/src/Components/SnackDetailComponent.js
+++ front-end/src/Components/SnackDetailComponent.js
@@ -1,7 +1,13 @@
 import axios from "axios"
 import {useState, useEffect} from "react"
+import {useParams} from "react-router-dom"
 
 function SnackDetail() {
+  const URL = process.env.REACT_APP_API_URL
+  const {id} = useParams()
+  
+  
+
     return (
       <div className="SnackDetail">
         Single Snack Detail card
