e5ec2aa75d2a164e3530ae1f912c9e679b239298
RohanPursuit <rohansterling@pursuit.org>
2022-02-16 03:04:45 -0500

    add main tag

--- front-end/src/Pages/AllSnacksPage.js
+++ front-end/src/Pages/AllSnacksPage.js
@@ -3,9 +3,12 @@ import "../Styles/AllSnacks.css"
 
 function AllSnacksPage() {
     return (
-      <div className="AllSnacksPage">
-        <AllSnacks/>
-      </div>
+      <main className="AllSnacksPage">
+        <section className="Snacks">
+          <article className="Snacks"><img src="#" alt="" /><div></div></article>
+          <AllSnacks/>
+        </section>
+      </main>
     );
 }
 
